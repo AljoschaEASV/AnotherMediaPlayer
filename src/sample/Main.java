d16a0fcbaa475cad8d46a0ba086e5432e0f8be5a
Aljoscha <aljo0025@easv365>
2020-01-08 13:25:27 +0100

    preloader(); method missing inside "Main extends Application"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class Main extends Application {
-   preloader();
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
